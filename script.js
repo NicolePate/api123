200e64e49a6c2867094e37d702d566707f6f1c86
NicolePate <nicole.pate512@gmail.com>
2022-02-09 20:28:42 -0600

    updated js

--- script.js
+++ script.js
@@ -1,9 +1,4 @@
 fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
   .then(response => response.json())
   .then(data => console.log(data));
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-  .then(response => response.json())
-  .then(data => console.log(data));
-  fetch('https://pokeapi.co/api/v2/pokemon?limit=100&offset=200')
-  .then(response => response.json())
-  .then(data => console.log(data));
+ 
